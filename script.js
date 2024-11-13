680e2759984093fa1c55a1667e2d9161f104890f
Marketa Havelkova <marketa.bartova195@gmail.com>
2024-11-13 17:50:43 +0100

    změna4

--- script.js
+++ script.js
@@ -19,18 +19,16 @@ const drahaVolba = (pocet) => {
     return `Catering od ${nazev} pro ${pocet} lidí za ${cena} Kč`;
 };
 
+console.log(levnaVolba(100));
+console.log(stredniVolba(100));
+console.log(drahaVolba(100));
+
 const createEvent = (nazevUdalosti, pocetLidi, funkceCatering) => {
     let cateringZprava = funkceCatering(pocetLidi);
     cateringZprava = cateringZprava.substring(0, 1).toLowerCase() + cateringZprava.substring(1);
     return `Událost ${nazevUdalosti} s ${cateringZprava}.`;
 };
 
-console.log(levnaVolba(100));
-console.log(stredniVolba(100));
-console.log(drahaVolba(100));
-
-
 console.log(createEvent("Inaugurace prezidenta", 100, drahaVolba));
 console.log(createEvent("Levná párty", 150, levnaVolba));
 console.log(createEvent("Svatba", 120, stredniVolba));
-
