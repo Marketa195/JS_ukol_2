123b89f6a39afae1b96ba692e98161d3fd4e6355
Markéta Havelková <406914@ucn.muni.cz>
2025-01-10 09:05:56 +0100

    test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const levnaVolba = (pocet) => {
     const nazev = 'Just Food';
-    const cenaZaOsobu = 100;
+    const cenaZaOsobu = 10000;
     const cena = pocet * cenaZaOsobu;
-    return `Catering od ${nazev} pro ${pocet} lidí za ${cena} Kč`;
+    return `Cateringy od ${nazev} pro ${pocet} lidí za ${cena} Kč`;
 };
 
 const stredniVolba = (pocet) => {
